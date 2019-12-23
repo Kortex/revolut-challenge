160bfb2cee37c216ae647516603145dce3704adc
Aris Kourtesas <ariskourt@gmail.com>
2019-12-23 21:42:04 +0200

    cleanup

--- src/main/java/com/ariskourt/revolut/services/AmountTransferStrategy.java
+++ src/main/java/com/ariskourt/revolut/services/AmountTransferStrategy.java
@@ -5,7 +5,7 @@ import com.ariskourt.revolut.domain.BankAccount;
 import java.util.function.BiConsumer;
 
 /***
- * Marker interface providing a unified type
+ * Marker interface extending BiConsumer providing a unified type between strategies
  */
 public interface AmountTransferStrategy extends BiConsumer<BankAccount, BankAccount> {
 
--- src/main/java/com/ariskourt/revolut/services/NegativeAmountStrategy.java
+++ src/main/java/com/ariskourt/revolut/services/NegativeAmountStrategy.java
@@ -15,6 +15,11 @@ class NegativeAmountStrategy implements AmountTransferStrategy {
 
     private final BigDecimal amount;
 
+    /***
+     * Accepts two account objects and transfer the amount between them
+     * @param from - The account to transfer from
+     * @param to - The account to transfer to
+     */
     @Override
     public void accept(BankAccount from, BankAccount to) {
 	log.info("Transferring {} funds from {} to {}", amount, from.getId(), to.getId());
